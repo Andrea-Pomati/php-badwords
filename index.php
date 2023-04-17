2ad67024fb38e7d4722496255c8c73041bf9ca4a
Andrea-Pomati <pomati.andrea@gmail.com>
2023-04-17 17:33:30 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Badwords</title>
+</head>
+<body>
+
+<h1>Inserisci il testo!</h1>
+
+
+<form action="soluzione.php" method="GET">
+   
+    <input type="textarea" placeholder="paragrafo" name="paragrafo" required>
+    <input type="text" name="parola" placeholder="parola" required>
+    <button type="submit">Invia i dati</button>
+  </form>
+
+
+
+</body>
+</html>
